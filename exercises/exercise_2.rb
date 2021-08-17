0574577531f6c726b13d5a09cbecc9de6e7414f6
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-17 12:42:42 -0400

    add exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# update store1 name to "Delta"
+@store1.name = "Delta"
+@store1.save
